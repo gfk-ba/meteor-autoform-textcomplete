0bf926133d4fc7e4eaf5ef78acdcbaf4079bdc2f
Peter Pal Koszta <peter.koszta@gfk.com>
2015-05-08 18:15:13 +0200

    add missing package version

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.onUse(function(api) {
     api.use('templating@1.0.0');
     api.use('blaze@2.0.0');
     api.use('aldeed:autoform@4.0.0 || 5.0.0');
-    api.use('mquandalle:jquery-textcomplete');
+    api.use('mquandalle:jquery-textcomplete@0.0.0');
 
     api.addFiles([
         'autoform-textcomplete.html',
