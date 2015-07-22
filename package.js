e623b19249ba45a94a338cf8ea3e3ece3423dd13
Marco de Jongh <marco.dejongh@gfk.com>
2015-07-22 12:13:50 +0200

    Upgraded textcomplete version

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
     name: 'gfk:autoform-textcomplete',
     summary: 'jQuery textcomplete for AutoForm',
-    version: '0.0.1'
+    version: '0.0.2'
 });
 
 Package.onUse(function(api) {
     api.use('templating@1.0.0');
     api.use('blaze@2.0.0');
     api.use('aldeed:autoform@4.0.0 || 5.0.0');
-    api.use('mquandalle:jquery-textcomplete@0.0.0');
+    api.use('mquandalle:jquery-textcomplete@0.3.9_1');
 
     api.addFiles([
         'autoform-textcomplete.html',
